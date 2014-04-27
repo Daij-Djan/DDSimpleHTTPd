800d3df809ebbd1e661377147d4426558bf60d89
Alex Gray <alex@mrgray.com>
2014-04-27 15:11:13 -0400

    light refactoring of mac-demo to make even MORE modern

--- DemoWebserver/AppDelegate.h
+++ DemoWebserver/AppDelegate.h
@@ -1,23 +1,16 @@
-//
-//  AppDelegate.h
-//  DemoWebserver
-//
-//  Created by Dominik Pich on 24.05.13.
-//
-//
+
+// AppDelegate.h DemoWebserver Created by Dominik Pich on 24.05.13.
 
 #import <Cocoa/Cocoa.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
-@property (assign) IBOutlet NSWindow *window;
-@property (weak) IBOutlet NSButton *toggle;
-
-@property (weak) IBOutlet NSTextField *name;
-@property (weak) IBOutlet NSTextField *port;
-@property (weak) IBOutlet NSTextField *webroot;
-@property (weak) IBOutlet NSButton *publishViaBonjour;
-
-- (IBAction)toggleListening:(id)sender;
+@property (assign) IBOutlet    NSWindow * window;
+@property (weak)   IBOutlet    NSButton * toggle,
+                                        * publishViaBonjour;
+@property (weak)   IBOutlet NSTextField * name,
+                                        * port,
+                                        * webroot;
+- (IBAction) toggleListening:(id)x;
 
 @end
